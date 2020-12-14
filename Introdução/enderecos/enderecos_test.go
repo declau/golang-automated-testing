b0ec67571373a9c89a431bc94ea133d60f0104ee
declau <dec82@hotmail.com>
2020-12-14 14:50:45 -0300

    different scenarios

--- Introdução/enderecos/enderecos_test.go
+++ Introdução/enderecos/enderecos_test.go
@@ -2,19 +2,42 @@ package enderecos
 
 import "testing"
 
-// Teste de unidade
+type cenarioDeteste struct {
+	enderecoInserido string
+	retornoEsperado  string
+}
 
 func TestTipoDeEndereco(t *testing.T) {
 	//Nome da Função começa obrigatóriamente com a palavra Test
-	enderecoParateste := "Avenida Paulista"
-	tipoDeEnderecoEsperado := "Avenida"
-	tipoDeEnderecoRecebido := TipoDeEndereco(enderecoParateste)
 
-	if tipoDeEnderecoRecebido != tipoDeEnderecoEsperado {
-		t.Errorf("Tipo diferente do esperado! Esperava o tipo %s e recebeu %s",
-			tipoDeEnderecoEsperado,
-			tipoDeEnderecoRecebido,
-		)
+	cenariosDeTeste := []cenarioDeteste{
+		{"Rua ABC", "Rua"},
+		{"Avenida Paulista", "Avenida"},
+		{"Rodovia dos Imigrantes", "Rodovia"},
+		{"Praça das Rosas", "Tipo inválido"},
+		{"Estrada Trem Baum", "Estrada"},
+		{"RUA DOS BOBOS", "Rua"},
+		{"AVENIDA JOÃO PINHEIRO", "Avenida"},
+		{"", "Tipo inválido"},
 	}
+
+	for _, cenario := range cenariosDeTeste {
+		retornoRecebido := TipoDeEndereco(cenario.enderecoInserido)
+		if retornoRecebido != cenario.retornoEsperado {
+			t.Errorf("Tipo de endereço recebido %s é diferente do esperado %s",
+				retornoRecebido,
+				cenario.retornoEsperado,
+			)
+		}
+	}
+
+	/*
+		//Usado para função básica
+		if tipoDeEnderecoRecebido != tipoDeEnderecoEsperado {
+			t.Errorf("Tipo diferente do esperado! Esperava o tipo %s e recebeu %s",
+				tipoDeEnderecoEsperado,
+				tipoDeEnderecoRecebido,
+			)
+		} */
 
 }
